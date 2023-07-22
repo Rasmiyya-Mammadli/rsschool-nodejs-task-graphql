726466f313152bc9c581a14b02d8fc7372be0ec1
Rasmiyya-Mammadli <rashababayeva@gmail.com>
2023-07-22 16:38:05 +0400

    feat:add some imports to index.ts file

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,55 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql } from 'graphql';
+import { buildSchema } from 'graphql';
+import { graphql, GraphQLResolveInfo } from 'graphql';
+import { UUIDType } from './types/uuid.js';
+
+// Sample data (you can replace this with your database or data source)
+const usersData: User[] = [];
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+}
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, GraphQL!',
+  },
+  Mutation: {
+    createUser: (args: { name: string; email: string }) => {
+      // For simplicity, generate a random UUID for the user ID (you can use a UUID library in your project)
+      const userId = 'SAMPLE_USER_ID'; // Replace this with a UUID library call
+      const newUser: User = {
+        id: userId,
+        name: args.name,
+        email: args.email,
+      };
+      usersData.push(newUser);
+      return newUser;
+    },
+  },
+};
+
+// Your custom GraphQL schema
+const gqlSchema = `
+  type Query {
+    hello: String
+  }
+
+  type Mutation {
+    createUser(name: String!, email: String!): User
+  }
+
+  type User {
+    id: UUID!
+    name: String!
+    email: String!
+  }
+`;
+
+const schema = buildSchema(gqlSchema);
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -13,7 +62,20 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
-      return {};
+      const { query, variables } = req.body;
+      const result = await graphql(
+        schema,
+        query,
+        resolvers,
+        undefined,
+        variables,
+        undefined,
+        undefined,
+        (info: GraphQLResolveInfo) => {
+       
+        }
+      );
+      return { ...result };
     },
   });
 };
