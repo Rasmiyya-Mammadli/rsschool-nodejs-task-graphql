f225acd81741d6dacad05b8211872f9f8ea8d256
Rasmiyya-Mammadli <rashababayeva@gmail.com>
2023-07-24 21:02:00 +0400

    feat: add task

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,63 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { buildSchema } from 'graphql';
-import { graphql, GraphQLResolveInfo } from 'graphql';
-import depthLimit from 'graphql-depth-limit'; 
-import { UUIDType } from './types/uuid.js';
-
-// Sample data (you can replace this with your database or data source)
-const usersData: User[] = [];
-
-interface User {
-  id: string;
-  name: string;
-  email: string;
-}
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hello, GraphQL!',
-  },
-  Mutation: {
-    createUser: (args: { name: string; email: string }) => {
-      // Generate a random UUID for the user ID using the UUIDType
-      const userId = UUIDType.serialize(generateRandomUUID());
-      const newUser: User = {
-        id: userId,
-        name: args.name,
-        email: args.email,
-      };
-      usersData.push(newUser);
-      return newUser;
-    },
-  },
-};
-
-
-const generateRandomUUID = (): string => {
-  // Implement your logic here to generate a random UUID.
-  // For simplicity, you can use a library or a custom function to generate the UUID.
-  // Replace this with your actual implementation to generate random UUIDs.
-  return 'SAMPLE_RANDOM_UUID';
-};
-// Your custom GraphQL schema
-const gqlSchema = `
-  type Query {
-    hello: String
-  }
-
-  type Mutation {
-    createUser(name: String!, email: String!): User
-  }
-
-  type User {
-    id: UUID!
-    name: String!
-    email: String!
-  }
-`;
-
-const schema = buildSchema(gqlSchema);
+import { graphql, parse, validate } from 'graphql';
+import depthLimit from 'graphql-depth-limit';
+import { createContext } from './context.js';
+import { createGqlResponseSchema, gqlResponseSchema, gqlSchema } from './schemas.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -70,21 +15,18 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
       },
     },
     async handler(req) {
+      const { prisma } = fastify;
       const { query, variables } = req.body;
-      const result = await graphql(
-        schema,
-        query,
-        resolvers,
-        undefined,
-        variables,
-        undefined,
-        undefined,
-        (info: GraphQLResolveInfo) => {
-          // Step 2: Use depthLimit as a validation rule to limit query complexity
-          return { validationRules: [depthLimit(5)] };
-        }
-      );
-      return { ...result };
+
+      const errors = validate(gqlSchema, parse(query), [depthLimit(5)]);
+      if (errors.length) return { errors };
+
+      return graphql({
+        schema: gqlSchema,
+        source: query,
+        variableValues: variables,
+        contextValue: createContext(prisma),
+      });
     },
   });
 };
